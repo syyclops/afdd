e2a0257f5d88ee921f76025a5f6632afadfa70bf
Jessica Klurfeld <jklurfeld@smith.edu>
2024-07-19 09:57:27 -0400

    setup neo4j, modified so fetches graph from neo4j

--- afdd/db.py
+++ afdd/db.py
@@ -3,6 +3,8 @@ from typing import List
 import pandas as pd
 from psycopg import Connection
 import json
+import neo4j
+from neo4j import GraphDatabase
 
 from afdd.models import PointReading, Rule, Condition, Metric, Severity
 from afdd.logger import logger
@@ -112,7 +114,7 @@ def load_timeseries(conn: Connection, graphInfoDF: pd.DataFrame, start_time: str
   Timestamp is the index, the columns contain the data of each timeseriesid.
   """
   # gets all of the timeseriesids that correspond to the given brick class
-  timeseries_ids = graphInfoDF.loc[graphInfoDF['class'] == URIRef(brick_class), "timeseriesid"].to_list()
+  timeseries_ids = graphInfoDF.loc[graphInfoDF['class'] == brick_class, "timeseriesid"].to_list()
 
   # converting timeseriesids to strings instead of literals so we can fetch them from the database
   timeseries_ids = [str(id) for id in timeseries_ids]
@@ -147,3 +149,19 @@ def load_timeseries(conn: Connection, graphInfoDF: pd.DataFrame, start_time: str
     logger.info(f"pivoted ts df: \n{df_pivoted}")
 
   return df_pivoted
+
+def load_graph_neo4j(driver: GraphDatabase.driver, component_class: str) -> pd.DataFrame:
+  """ Loads all necessary sensor information for the given component for a neo4j graph """
+
+  query = """
+  MATCH (c:Class) WHERE c.uri CONTAINS $component_class
+  MATCH (c)-[:HAS_BRICK_CLASS]-(comp:Component)
+  MATCH (comp)-[:hasPoint]-(p:Point)
+  MATCH (comp)-[:isDeviceOf]-(d:Device)
+  MATCH (p)-[:HAS_BRICK_CLASS]-(class: Class)
+  MATCH (p)-[:hasExternalReference]-(t:TimeseriesReference)
+  RETURN p.uri AS point, class.uri AS class, t.hasTimeseriesId AS timeseriesid, d.uri AS deviceURI, comp.uri AS componentURI
+  """
+  
+  df = driver.execute_query(query_=query, component_class=component_class, database_="neo4j", result_transformer_=neo4j.Result.to_df)
+  return df
--- tests/scratch.py
+++ tests/scratch.py
@@ -6,6 +6,10 @@ import pandas as pd
 import os
 from enum import Enum
 from typing import List
+import neo4j
+from neo4j import GraphDatabase
+from afdd.utils import load_graph
+from dotenv import load_dotenv
 
 @dataclass
 class Metric(Enum):
@@ -68,67 +72,83 @@ combo_rule = Rule(
   )
 )
 
-data = {"device_id_pm25": [25, 35, 46, 90],
-        "device_id_pm10": [45, 50, 60, 43]}
+# data = {"device_id_pm25": [25, 35, 46, 90],
+#         "device_id_pm10": [45, 50, 60, 43]}
 
-date_range = pd.date_range('2024-07-01', periods=4, freq='H')
+# date_range = pd.date_range('2024-07-01', periods=4, freq='H')
 
-# equation_str = "({PM2.5_Level_Sensor} + {PM10_Level_Sensor}) > 100"
+# # equation_str = "({PM2.5_Level_Sensor} + {PM10_Level_Sensor}) > 100"
 
-df = pd.DataFrame(data, index=date_range)
-print(f"df: \n{df}")
+# df = pd.DataFrame(data, index=date_range)
+# # print(f"df: \n{df}")
 
-sensor_type_map = {"PM2.5_Level_Sensor": "pm25", "PM10_Level_Sensor": "pm10", "Temperature_Sensor": "temp", "CO2_Sensor": "co2", "TVOC_Level_Sensor": "tvoc", "Relative_Humidity_Sensor": "humidity"}
+# sensor_type_map = {"PM2.5_Level_Sensor": "pm25", "PM10_Level_Sensor": "pm10", "Temperature_Sensor": "temp", "CO2_Sensor": "co2", "TVOC_Level_Sensor": "tvoc", "Relative_Humidity_Sensor": "humidity"}
 
-# Create a dictionary to map sensor types to column names
-column_name_map = {sensor_type_map[sensor]: f"device_id_{sensor_type_map[sensor]}" for sensor in combo_rule.sensor_types}
-hc_column_name_map = {
-  "pm10":"device_id_pm10",
-  "pm25":"device_id_pm25"
+# # Create a dictionary to map sensor types to column names
+# column_name_map = {sensor_type_map[sensor]: f"device_id_{sensor_type_map[sensor]}" for sensor in combo_rule.sensor_types}
+# hc_column_name_map = {
+#   "pm10":"device_id_pm10",
+#   "pm25":"device_id_pm25"
+# }
+# # print(f"sensor map: {column_name_map}")
+
+# # Use the sensor_map to format the equation string
+# formatted_equation = combo_rule.condition.equation.format(**column_name_map)
+# # print(f"formatted equation: {formatted_equation}")
+
+# # Evaluate the formatted equation
+# df['results'] = df.eval(formatted_equation)
+
+# # print(df)
+
+env_files = {
+'local': '.env',
+'dev': '.env.dev'
 }
-print(f"sensor map: {column_name_map}")
+load_dotenv()
+try:
+  env_file = env_files[os.environ['ENV']]
+except Exception:
+  env_file = env_files['local']  
+load_dotenv(env_file, override=True)
 
-# Use the sensor_map to format the equation string
-formatted_equation = combo_rule.condition.equation.format(**column_name_map)
-print(f"formatted equation: {formatted_equation}")
+# URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
+URI = os.environ['NEO4J_URI']
+AUTH = (os.environ['NEO4J_USER'], os.environ['NEO4J_PASSWORD'])
+print(f"URI: {URI}")
 
-# Evaluate the formatted equation
-df['results'] = df.eval(formatted_equation)
+query = """
+MATCH (c:Class) where c.uri contains "IAQ_Sensor_Equipment" 
+    MATCH (c)-[:HAS_BRICK_CLASS]-(comp:Component)
+    MATCH (comp)-[:hasPoint]-(p:Point)
+    MATCH (comp)-[:isDeviceOf]-(d:Device)
+    MATCH (p)-[:HAS_BRICK_CLASS]-(class: Class)
+    MATCH (p)-[:hasExternalReference]-(t:TimeseriesReference)
+    RETURN p.uri AS point, class.uri AS class, t.hasTimeseriesId AS timeseriesid, d.uri AS deviceURI, comp.uri AS componentURI
+"""
 
-print(df)
+with GraphDatabase.driver(URI, auth=AUTH) as driver:
+    driver.verify_connectivity()
+    df = driver.execute_query(query_=query, database_="neo4j", result_transformer_=neo4j.Result.to_df)
+    print(df)
 
-# name = 'jessica'
-# x = 'print(f"my name is: {name}")'
-# # exec(x, name)
+# print(load_graph("kaiterra_dcoffice.ttl")["point"])
 
-# name = 'jessica'
-# x = f'print(f"my name is: {name}")'
-# exec(x)
+"""
+MATCH (c:Component)-[:hasPoint]-(p:Point)
+            MATCH (c)-[:HAS_BRICK_CLASS]-(b:Class) where toLower(b.uri)contains "iaq"
+            OPTIONAL MATCH (p)-[:hasUnit]->(u:Unit)
+            OPTIONAL MATCH (p)-[:hasExternalReference]->(ts:TimeseriesReference)
+            RETURN c as Component, p as Point, u, ts
 
-# y = "print('my name is:', name)"
-# exec(y)
+MATCH (c:Class) where c.uri contains "IAQ_Sensor_Equipment" 
+    MATCH (c)-[:HAS_BRICK_CLASS]-(comp:Component)
+    MATCH (comp)-[:hasPoint]-(p:Point)
+    MATCH (comp)-[:isDeviceOf]-(d:Device)
+    MATCH (p)-[:HAS_BRICK_CLASS]-(class: Class)
+    MATCH (p)-[:hasExternalReference]-(t:TimeseriesReference)
+    RETURN p.uri AS point, class.uri AS class, t.hasTimeseriesId AS timeseriesid, d.uri AS deviceURI, comp.uri AS componentURI
 
-# txt = "For only {price:.2f} dollars!"
-# print(txt.format(price = 49))
 
-# # temp = 200
-# equation = "{temp} > 100"
-# # if exec(equation.format(temp=temp)):
-# #   print('anomaly!')
-
-# exec("temp=500\nif equation.format(temp=temp):\n\tprint('anomaly!', temp)")
-
-# string = ""
-# for sensor in sensor_types:
-#   string += f"{sensor} = \"device_id-{sensor}\","
-  
-# string = string[:-1]
-# print(f"string: {string}")
-
-# # Evaluate the equation string as a Python expression
-# df['results'] = df.eval(equation_str.format(exec(string)))
-
-# # df['results'] = df.eval(equation_str.format(pm10))
-
-# print(df['results'])
-
+"""
+ 
