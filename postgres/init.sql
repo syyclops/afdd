e9dc8f2739903b9817fc16ac22fe46d870a7be55
Silin Yi <silin.yi@setty.com>
2024-08-01 10:25:49 -0400

    changed init.sql to format tables correctly

--- postgres/init.sql
+++ postgres/init.sql
@@ -7,10 +7,10 @@ CREATE TABLE timeseries (
 CREATE TABLE rules (
   rule_id INT PRIMARY KEY,
   name TEXT NOT NULL,
-  component_type TEXT NOT NULL,
-  sensor_types JSONB NOT NULL,
   description TEXT NOT NULL,
-  condition JSONB NOT NULL
+  condition JSONB NOT NULL,
+  component_type TEXT NOT NULL,
+  sensor_types JSONB NOT NULL
 );
 
 CREATE TABLE anomalies (
@@ -18,8 +18,8 @@ CREATE TABLE anomalies (
   end_time TIMESTAMPTZ NOT NULL,
   rule_id INT,
   anomaly_id INT GENERATED ALWAYS AS IDENTITY,
-  points JSONB NOT NULL,
   metadata JSONB NOT NULL,
+  points JSONB NOT NULL,
   PRIMARY KEY(anomaly_id),
   CONSTRAINT fk_rules
     FOREIGN KEY(rule_id) 
